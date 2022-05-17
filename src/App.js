e6030cc6e8e932eb584f004d5fc3cf7f71b7e822
unknown <anoopachenkunju555@gmail.com>
2022-05-17 10:48:44 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addstudent from './components/Addstudent';
 import Searchstudent from './components/Searchstudent';
+
 function App() {
   return (
     <>
 
    <Addstudent/>
 <Searchstudent/>
+
 </>
    
     
